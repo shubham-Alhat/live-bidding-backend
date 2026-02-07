375e0aa739d6d42a3331fb9a5a0b7492ea7cfdc7
shubham-Alhat <shubhamalhat520@gmail.com>
2026-02-07 21:39:28 +0530

    basic controllers done

--- src/controllers/bid.controller.ts
+++ src/controllers/bid.controller.ts
@@ -1,8 +1,16 @@
 import type { Request, Response } from "express";
+import { prisma } from "../db/prisma.js";
 
-export const createNewBid = (req: Request, res: Response) => {
+export const createNewBid = async (req: Request, res: Response) => {
   try {
     const user = req.authUser;
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found in middleware", data: null });
+    }
+
     const { price, auctionId } = req.body;
 
     if (!price || !auctionId) {
@@ -10,6 +18,20 @@ export const createNewBid = (req: Request, res: Response) => {
         .status(404)
         .json({ message: "price or auctionId not found", data: null });
     }
+
+    const newBid = await prisma.bid.create({
+      data: {
+        price: price,
+        bidderId: user.id,
+        auctionId: auctionId,
+      },
+      include: {
+        bidder: true,
+        auction: true,
+      },
+    });
+
+    return res.status(201).json({ message: "new bid created", data: newBid });
   } catch (error) {
     console.log(error);
     return res
