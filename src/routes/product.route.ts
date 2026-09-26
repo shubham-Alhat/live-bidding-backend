a33d7ed8aab97f019b3cb559ed0fa5781b2bc21c
shubham-Alhat <shubhamalhat520@gmail.com>
2026-09-26 11:32:59 +0530

    refactor: comment out product route methods for future implementation

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,23 +1,23 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
-import {
-  createNewProduct,
-  deleteProduct,
-  getAllProducts,
-  getTheProduct,
-  launchProduct,
-} from "../controllers/product.controller.js";
+// import {
+//   createNewProduct,
+//   deleteProduct,
+//   getAllProducts,
+//   getTheProduct,
+//   launchProduct,
+// } from "../controllers/product.controller.js";
 
 const router = Router();
 
-router
-  .route("/create")
-  .post(authMiddleware, upload.single("image"), createNewProduct);
+// router
+//   .route("/create")
+//   .post(authMiddleware, upload.single("image"), createNewProduct);
 
-router.route("/launch/:productId").put(authMiddleware, launchProduct);
-router.route("/:productId").delete(authMiddleware, deleteProduct);
-router.route("/get-all").get(authMiddleware, getAllProducts);
-router.route("/:id").get(authMiddleware, getTheProduct);
+// router.route("/launch/:productId").put(authMiddleware, launchProduct);
+// router.route("/:productId").delete(authMiddleware, deleteProduct);
+// router.route("/get-all").get(authMiddleware, getAllProducts);
+// router.route("/:id").get(authMiddleware, getTheProduct);
 
 export default router;
